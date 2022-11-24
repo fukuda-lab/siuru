768cf52791d5d18487bceb280fa1cdece4a0bf15
Laura Lahesoo <l.lahesoo@klebert-engineering.de>
2022-11-24 13:59:32 +0900

    stop_kafka_pcap: Fix timestamp.

--- code/stop_kafka_pcap.bash
+++ code/stop_kafka_pcap.bash
@@ -2,7 +2,6 @@
 # Delay setting errexit because "docker-compose down" does not like to exit with 0.
 set -o nounset
 set -o pipefail
-set -x
 
 # TODO make the volume name configurable over env, replace all references to pcap-data.
 
@@ -16,12 +15,13 @@ set -o errexit
 
 # Extract data and store it neatly, then get rid of the Docker volume.
 timestamp=$(date +%s)
-data_dir="$CODE_DIR/../data/${timestamp}-pcap-data"
+datetime=$(date +"%F-%T")
+data_dir="$CODE_DIR/../data/${datetime}-pcap-data"
 mkdir "$data_dir"
 
-container_name="temp-$timestamp"
+container_name="temp-${timestamp}"
 docker container create --name $container_name -v pcap-data:/data hello-world
-docker cp $container_name:/data "$data_dir/"
+docker cp $container_name:/data "$data_dir"
 docker stop "$container_name"
 docker rm "$container_name"
 docker volume rm pcap-data
