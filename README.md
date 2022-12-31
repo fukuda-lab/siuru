11389197ac93a4e46305fcaf73daea05f8408172
Laura Lahesoo <l.lahesoo@klebert-engineering.de>
2022-12-31 17:49:16 +0900

    code: WIP feature extractor commandline tool in C++.

--- README.md
+++ README.md
@@ -7,3 +7,27 @@ Pcap files stored with timestamps. Data is automatically moved here when you run
 `code/stop_kafka_pcap.bash` script.
 
 While timestamps in pcap file names are in UST, packets inside have JST timestamps (+9).
+
+### Working with large files
+
+Split up large pcap files before trying to load them with Scapy:
+
+```bash
+editcap -c 100000 200608241400.dump 200608241400_split.pcap
+```
+
+Extract packets in range 1 to 1000 (inclusive) from a capture file:
+
+```bash
+editcap -r 200608241400.dump 200608241400_part_1_1000.dump 1-1000
+```
+
+``editcap`` is a command line tool distributed with Wireshark.
+See [usage manual](https://www.wireshark.org/docs/man-pages/editcap.html) for details.
+
+## C++ feature extraction
+
+Installed dependencies:
+```bash
+libpcap0.8 libpcap0.8-dev libpcap0.8-dbg
+```
---
+++ code/cpp-extract-features/main.cpp
@@ -0,0 +1,77 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <pcap.h>
+
+auto& USAGE_STR = "Usage: ./pcap-feature-extraction [--test] [--file </path/to/file>]\n";
+
+int test_pcap() {
+  char errbuf[PCAP_ERRBUF_SIZE];
+  pcap_if_t *devs;
+  pcap_if_t dev;
+  int ret_code;
+  struct pcap_pkthdr header {};
+  const u_char *packet;
+
+  ret_code = pcap_findalldevs(&devs, errbuf);
+  fprintf(stdout, "pcap_findalldevs exited with code: %d\n", ret_code);
+  if (devs == nullptr) {
+    fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+    return (2);
+  }
+
+  // TODO select dev manually or from arguments.
+  dev = devs[0];
+  printf("Device: %s\n", dev.name);
+
+  pcap_t *handle;
+  handle = pcap_open_live(dev.name, BUFSIZ, 0, 1000, errbuf);
+  if (handle == nullptr) {
+    fprintf(stderr, "Couldn't open device %s: %s\n", dev.name, errbuf);
+    return (2);
+  }
+
+  packet = pcap_next(handle, &header);
+  printf("Got a packet with length of [%d]\n", header.len);
+  if (header.len != 0) {
+    printf("%s\n", packet);
+  }
+  pcap_close(handle);
+  return (0);
+}
+
+int read_from_file(const char* path) {
+  char errbuf[PCAP_ERRBUF_SIZE];
+  pcap_t *handle;
+  const u_char *packet;
+  struct pcap_pkthdr header {};
+  handle = pcap_open_offline(path, errbuf);
+  if (handle == nullptr) {
+    fprintf(stderr, "Couldn't open pcap file at %s: %s\n", path, errbuf);
+    return (2);
+  }
+
+  packet = pcap_next(handle, &header);
+  printf("Got a packet with length of [%d]\n", header.len);
+  if (header.len != 0) {
+    printf("%s\n", packet);
+  }
+  pcap_close(handle);
+  return (0);
+}
+
+int main(int argc, char *argv[]) {
+  for (auto i = 0; i < argc; i++) {
+    if (strcmp(argv[i], "--test") == 0) {
+      test_pcap();
+    }
+    if (strcmp(argv[i], "--file") == 0) {
+      if (i == argc - 1) {
+        std::cout << USAGE_STR;
+        exit (1);
+      }
+      i++;
+      read_from_file(argv[i]);
+    }
+  }
+}
