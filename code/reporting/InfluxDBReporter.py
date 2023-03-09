3ab2e6e9cf567b7e9304eb3ce51e93f507340191
Laura Lahesoo <l.lahesoo@klebert-engineering.de>
2023-03-09 19:25:35 +0900

    code: InfluxDB reporter is functional.

--- code/reporting/InfluxDBReporter.py
+++ code/reporting/InfluxDBReporter.py
@@ -7,7 +7,9 @@ from preprocessors.common import PacketFeature
 
 import pipeline_logger
 
-stop = 20000
+
+log = pipeline_logger.PipelineLogger.get_logger()
+
 
 # TODO Create reporter interface.
 class InfluxDBReporter:
@@ -27,12 +29,14 @@ class InfluxDBReporter:
         # TODO optimize batch reporting (the default).
         # 2023-03-02 Performance with synchronous reporting: 87 packets/s.
         # 2023-03-02 Performance with default batch reporting: 168 packets/s.
-        self.write_api = self.client.write_api(success_callback=self.success,
-                                               error_callback=self.error,
-                                               retry_callback=self.retry
-                                               )
-        self.logger.info(f"Initialized InfluxDB writer to {self.url} "
-                 f"[{self.org}/{self.bucket}].")
+        self.write_api = self.client.write_api(
+            success_callback=self.success,
+            error_callback=self.error,
+            retry_callback=self.retry
+            )
+        self.logger.info(
+            f"Initialized InfluxDB writer to {self.url} "
+            f"[{self.org}/{self.bucket}].")
 
     def success(self, conf: (str, str, str), data: str):
         pass
@@ -44,15 +48,15 @@ class InfluxDBReporter:
         self.logger.info(f"Retryable error occurs for batch: {conf}, data: {data} retry: {exception}")
 
     def report(self, p: Prediction, measurement_name: str = "anomaly_detection"):
-        global stop
-
         point = Point(measurement_name)
         point.time(p.fields[PacketFeature.TIMESTAMP].isoformat(timespec="nanoseconds"))
-        point.tag(PacketFeature.IP_SOURCE_ADDRESS.value, p.fields[PacketFeature.IP_SOURCE_ADDRESS])
-        point.tag(PacketFeature.IP_SOURCE_PORT.value, p.fields[PacketFeature.IP_SOURCE_PORT])
-        point.tag(PacketFeature.IP_DESTINATION_ADDRESS.value, p.fields[PacketFeature.IP_DESTINATION_ADDRESS])
-        point.tag(PacketFeature.IP_DESTINATION_PORT.value, p.fields[PacketFeature.IP_DESTINATION_PORT])
-        point.tag(PacketFeature.PROTOCOL.value, p.fields[PacketFeature.PROTOCOL])
+        point.tag(PredictionField.MODEL_NAME.value, p.fields[PredictionField.MODEL_NAME.value])
+        # TODO Decide on relevant tags, or leave them customizable.
+        # point.tag(PacketFeature.IP_SOURCE_ADDRESS.value, p.fields[PacketFeature.IP_SOURCE_ADDRESS])
+        # point.tag(PacketFeature.IP_SOURCE_PORT.value, p.fields[PacketFeature.IP_SOURCE_PORT])
+        # point.tag(PacketFeature.IP_DESTINATION_ADDRESS.value, p.fields[PacketFeature.IP_DESTINATION_ADDRESS])
+        # point.tag(PacketFeature.IP_DESTINATION_PORT.value, p.fields[PacketFeature.IP_DESTINATION_PORT])
+        # point.tag(PacketFeature.PROTOCOL.value, p.fields[PacketFeature.PROTOCOL])
 
         if PredictionField.OUTPUT_BINARY in p.fields:
             point.field(PredictionField.OUTPUT_BINARY.value, p.fields[PredictionField.OUTPUT_BINARY.value])
@@ -60,9 +64,3 @@ class InfluxDBReporter:
             raise NotImplementedError("TODO: Support other prediction outputs!")
 
         self.write_api.write(bucket=self.bucket, org=self.org, record=point)
-        stop -= 1
-        if stop <= 0:
-            print("Early stopping...")
-            exit(0)
-        elif stop % 5000 == 0:
-            print(point)
