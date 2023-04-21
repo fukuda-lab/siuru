0c7524ac7748019842ce293abb15659310e91dd7
Laura Lahesoo <l.lahesoo@klebert-engineering.de>
2023-04-21 11:40:52 +0200

    code: Two-mode split_dataset.bash script completed.

--- code/split_dataset.bash
+++ code/split_dataset.bash
@@ -3,69 +3,106 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# TODO limiting the split count means packets will be written in a round-robin policy.
-# Instead, we may wish to split X first flows to test, then X to validation, then test.
-# Easiest way is to not limit the file count (each flow is written into a separate file)
-# and calculate the number of flows for each set after splitting finishes.
+valid_args=true
 
-if [[ $# != 5 || "$1" == "--help" ]]; then
+if [[ "$1" != "head-tail" && "$1" != "round-robin" ]]; then
+    valid_args=false
+fi
+if [[ "$1" == "head-tail" && $# != 5 ]]; then
+    valid_args=false
+elif [[ "$1" == "round-robin" && $# != 6 ]]; then
+    valid_args=false
+fi
+
+if [[ $valid_args != "true" ]]; then
+    echo "Provided parameters don't match the expected signature! Here's help:"
     echo ""
-    echo "Usage: ./split_dataset.bash <input-file> <output-dir> <split-count> <train-count> <validation-count>"
+    echo "========================== split_dataset.bash ============================"
+    echo "Split captured packets by flow into a training, validation, and test set."
+    echo "========================================================================="
     echo ""
-    echo "Split a PCAP file to #(split-count) files by flow, then merge them into a"
-    echo "train-validation-test set stored as [train|validate|test].pcap in the output directory."
-    echo "The training set will contain flows from the first #(train-count) files,"
-    echo "validation the next #(validation-count) files, and the remaining files will be in test."
+    echo "Usage: ./split_dataset.bash <head-tail|round-robin> <args ...>"
+    echo "Args head-tail: <input-file> <output-dir> <%train> <%validation>"
+    echo "Args round-robin: <input-file> <output-dir> <#split> <#train> <#validation>"
+    echo ""
+    echo "head-tail: Writes the flows in input data to separate files (can be many!),"
+    echo "then merges them into [train|validate|test].pcapng files in the output dir."
+    echo "train.pcapng will contain the first <%train> percent of all created files,"
+    echo "validation the next <%validation> percent, remaining ones will be in test."
+    echo ""
+    echo "round-robin: Writes the packets to <#split> files, then merges them into"
+    echo "[train|validate|test].pcapng files in the output directory."
+    echo "train.pcapng will contain flows from the first <#train> files,"
+    echo "validation the next <#validation> files, remaining ones will be in test."
+    echo "The file for each flow is assigned circularly, hence the name round-robin."
     echo ""
     exit 1
 fi
 
-pcap_path=$1
-output_path=$2
-split_count=$3
-train_set=$4
-validation_set=$5
-test_set=$((split_count - train_set - validation_set))
+mode=$1
+pcap_path=$2
+output_path=$3
 
-mkdir -p $output_path
+mkdir $output_path
+tmp_dir=$output_path/pcapsplitter-tmp
+mkdir $tmp_dir
 
 # TODO Handle non-UDP/TCP packets if you need to work with datasets that contain them!
 # All packets which aren't UDP or TCP (hence don't belong to any connection) will be
 # written to one output file, separate from the other output files (usually file#0).
-PcapSplitter -f $pcap_path -o $output_path -m connection -p $split_count
+
+if [[ "$mode" == "round-robin" ]]; then
+    split_count=$4
+    train_set=$5
+    validation_set=$6
+    test_set=$((split_count - train_set - validation_set))
+
+    PcapSplitter -f $pcap_path -o $tmp_dir -m connection -p $split_count
+else
+    train_percent=$4
+    validation_percent=$5
+    test_percent=$((100 - train_percent - validation_percent))
+
+    PcapSplitter -f $pcap_path -o $tmp_dir -m connection
+
+    file_count=$(ls -1q $tmp_dir | wc -l)
+    train_set=$(echo "$file_count * $train_percent / 100" | bc)
+    validation_set=$(echo "$file_count * $validation_percent / 100" | bc)
+    test_set=$((file_count - train_set - validation_set))
+fi
 
 train_files=()
 validation_files=()
 test_files=()
 
-for f in $(ls $output_path); do
+for f in $(ls $tmp_dir); do
     if ((0 < train_set--)); then
-        train_files+=($output_path/$f)
+        train_files+=($tmp_dir/$f)
     elif ((0 < validation_set--)); then
-        validation_files+=($output_path/$f)
+        validation_files+=($tmp_dir/$f)
     else
-        test_files+=($output_path/$f)
+        test_files+=($tmp_dir/$f)
     fi
 done
 
-echo
-echo Training set:
-for f in ${train_files[@]}; do
-    echo "    $f"
-done
+readme_file="$output_path/README.md"
+touch "$readme_file"
 
-echo
-echo Validation set:
-for f in ${validation_files[@]}; do
-    echo "    $f"
-done
-
-echo
-echo Test set:
-for f in ${test_files[@]}; do
-    echo "    $f"
-done
+echo "PCAP files created with split_dataset.bash" >> "$readme_file"
+echo "Mode: $mode" >> "$readme_file"
+echo "Source file: $pcap_path" >> "$readme_file"
+if [[ "$mode" == "round-robin" ]]; then
+    echo "Files in training set: ${#train_files[@]}" >> "$readme_file"
+    echo "Files in validation set: ${#validation_files[@]}" >> "$readme_file"
+    echo "Files in test set: ${#test_files[@]}" >> "$readme_file"
+else
+    echo "Flows in training set: ${#train_files[@]}" >> "$readme_file"
+    echo "Flows in validation set: ${#validation_files[@]}" >> "$readme_file"
+    echo "Flows in test set: ${#test_files[@]}" >> "$readme_file"
+fi
 
 mergecap -w $output_path/train.pcapng ${train_files[@]}
 mergecap -w $output_path/validation.pcapng ${validation_files[@]}
 mergecap -w $output_path/test.pcapng ${test_files[@]}
+
+rm -r $tmp_dir
